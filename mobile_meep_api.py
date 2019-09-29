61d85e67ae5c8c25bbc109a3f37117059cb0728c
Vladislav <novitsky.vladislav00@gmail.com>
2019-09-30 02:42:54 +0300

    Added hardcoded straight waveguide implementation

--- mobile_meep_api.py
+++ mobile_meep_api.py
@@ -1,10 +1,56 @@
 from flask import Flask, jsonify
+import meep as mp
+import numpy as np
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+	return 'Hey, it`s working!'
+
+
+@app.route('/test')
 def test():
-    return jsonify(
-        test_data='test_string'
-    )
+	cell = mp.Vector3(16, 8, 0)
+
+	geometry = [mp.Block(mp.Vector3(mp.inf, 1, mp.inf),
+						 center=mp.Vector3(),
+						 material=mp.Medium(epsilon=12))]
+
+	sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+						 component=mp.Ez,
+						 center=mp.Vector3(-7, 0))]
+
+	pml_layers = [mp.PML(1.0)]
+
+	resolution = 10
+
+	sim = mp.Simulation(cell_size=cell,
+						boundary_layers=pml_layers,
+						geometry=geometry,
+						sources=sources,
+						resolution=resolution)
+
+	sim.run(until=200)
+
+	eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+
+	ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+	plt.figure()
+	plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+	plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+	plt.axis('off')
+	fig_electric = plt.gcf()
+
+	buffer = BytesIO()
+	fig_electric.savefig(buffer, format='png')
+	buffer.seek(0)
+	electric_b64 = base64.b64encode(buffer.read()).decode()
+	buffer.close()
+
+	return jsonify(
+		electric = electric_b64
+	)
