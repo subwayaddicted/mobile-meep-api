bdbd34114efdba5747f5d012b2aa1e51aaaec3e9
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2019-10-16 20:17:05 +0300

    Added png out to directory

--- mobile_meep_api.py
+++ mobile_meep_api.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import h5py
 
 app = Flask(__name__)
 api = Api(app, version='0.1', title='meep API', description='meep package used as API')
@@ -40,23 +41,31 @@ class StraightWaveguide(Resource):
 			sources=sources,
 			resolution=resolution)
 
-		sim.run(until=200)
+		sim.use_output_directory()
 
-		eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+		sim.run(mp.at_every(0.6, mp.output_png(mp.Ez, "-Zc /home/NIX/novitsky/PycharmProjects/mobile-meep-api/colormaps/dkbluered")), until=200)
 
-		ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
-		plt.figure()
-		plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-		plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
-		plt.axis('off')
-		fig_electric = plt.gcf()
+		# sim.run(
+		# 	mp.at_beginning(mp.output_epsilon),
+		# 	mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
+		# 	until=200
+		# )
 
-		buffer = BytesIO()
-		fig_electric.savefig(buffer, format='png')
-		buffer.seek(0)
-		electric_b64 = base64.b64encode(buffer.read()).decode()
-		buffer.close()
+		# eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+
+		# ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+		# plt.figure()
+		# plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+		# plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+		# plt.axis('off')
+		# fig_electric = plt.gcf()
+		#
+		# buffer = BytesIO()
+		# fig_electric.savefig(buffer, format='png')
+		# buffer.seek(0)
+		# electric_b64 = base64.b64encode(buffer.read()).decode()
+		# buffer.close()
 
 		return jsonify(
-			electric=electric_b64
+			electric='test message atm 3'
 		)
