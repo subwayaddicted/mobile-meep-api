b2c5dd887e583a3ee66eba27490165702a718cb8
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2019-10-18 18:46:15 +0300

    Stash applied

--- mobile_meep_api.py
+++ mobile_meep_api.py
@@ -90,6 +90,50 @@ class NinetyBend(Resource):
 
 		sim.run(mp.at_every(0.6, mp.output_png(mp.Ez, "-Zc" + colormap)), until=200)
 
+		image_transformer = ImageTransformer(dir_out)
+		image_transformer.png_to_gif()
+
+		return jsonify(
+			electric='test message atm 51'
+		)
+
+
+@ns_waveguides.route('/ninety-bend')
+class NinetyBend(Resource):
+	@api.doc('Returns test text and computes of e/m wave propagation in 90 bend waveguide')
+	def get(self):
+		dir_out = 'mobile-meep-out/'
+		colormap = ' /home/NIX/novitsky/PycharmProjects/mobile-meep-api/colormaps/dkbluered'
+
+		cell = mp.Vector3(16, 16, 0)
+		geometry = [mp.Block(
+			mp.Vector3(12, 1, mp.inf),
+			center=mp.Vector3(-2.5, -3.5),
+			material=mp.Medium(epsilon=12)),
+			mp.Block(
+				mp.Vector3(1, 12, mp.inf),
+				center=mp.Vector3(3.5, 2),
+				material=mp.Medium(epsilon=12))]
+		pml_layers = [mp.PML(1.0)]
+		resolution = 10
+
+		sources = [mp.Source(
+			mp.ContinuousSource(wavelength=2 * (11 ** 0.5), width=20),
+			component=mp.Ez,
+			center=mp.Vector3(-7, -3.5),
+			size=mp.Vector3(0, 1))]
+
+		sim = mp.Simulation(
+			cell_size=cell,
+			boundary_layers=pml_layers,
+			geometry=geometry,
+			sources=sources,
+			resolution=resolution)
+
+		sim.use_output_directory(dir_out)
+
+		sim.run(mp.at_every(0.6, mp.output_png(mp.Ez, "-Zc" + colormap)), until=200)
+
 		return jsonify(
 			electric='test message atm 52'
 		)
