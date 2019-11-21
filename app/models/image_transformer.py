b714edfa67035d525af7fc60cebf04299597187d
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2019-11-21 13:51:02 +0200

    Revert "Added type hints"
    This reverts commit 4a3d0cf11782179a76a3bfe5c356d615d0046444.

--- app/models/image_transformer.py
+++ app/models/image_transformer.py
@@ -4,14 +4,14 @@ import imageio
 
 class ImageTransformer:
 
-    def __init__(self, dir_out: str):
+    def __init__(self, dir_out):
         self.png_dir = dir_out
         self.images = []
         self.gif_dir = dir_out + '-' + 'movie.gif'
 
-    def png_to_gif(self, duration: int):
+    def png_to_gif(self):
         for file_name in os.listdir(self.png_dir):
             if file_name.endswith('.png'):
                 file_path = os.path.join(self.png_dir, file_name)
                 self.images.append(imageio.imread(file_path))
-        imageio.mimsave(self.gif_dir, self.images, format='GIF', duration=duration)
+        imageio.mimsave(self.gif_dir, self.images, format='GIF', duration=0.5)
