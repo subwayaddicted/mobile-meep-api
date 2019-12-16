c5acb0423fbcacf50604b6d9045817793e469082
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2019-12-16 18:46:20 +0200

    Added cell json model

---
+++ app/meep_api/models/json/cell.py
@@ -0,0 +1,21 @@
+cell_json_model = {
+    "cell": {
+        "type": "object",
+        "properties": {
+            "x": {
+                "type": "string"
+            },
+            "y": {
+                "type": "string"
+            },
+            "z": {
+                "type": "string"
+            }
+        },
+        "required": [
+            "x",
+            "y",
+            "z"
+        ]
+    }
+}
